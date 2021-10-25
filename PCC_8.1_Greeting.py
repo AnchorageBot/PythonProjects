f55130e066e84a4d32493ce58e77c4b55c8f3ecd
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2021-10-24 20:28:32 -0800

    Create PCC_8.1_Greeting.py

---
+++ PCC_8.1_Greeting.py
@@ -0,0 +1,11 @@
+# This function will greet a user
+
+# Source code/inspiration/software
+    # Python Crash Course by Eric Matthews, Chapter 8, Example 1
+    # Made with Mu 1.0.3 in October 2021
+
+def greet_user():                       # def is a Python keyword for defining a function
+    """"Greet a user"""                 # Python uses this to generate documentation for the function
+    print("Hello!")
+
+greet_user()                            # function call telling Python to execute the function
