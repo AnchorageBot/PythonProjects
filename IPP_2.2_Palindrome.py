e1ff736e0f9884679ca3f45aa414f1c97edc72f1
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2021-11-14 10:11:25 -0900

    Update and rename IPP_1.2_Palindrome.py to IPP_2.2_Palindrome.py

--- IPP_2.2_Palindrome.py
+++ IPP_2.2_Palindrome.py
@@ -1,7 +1,7 @@
 # This script will load a text file and attempt to find palindromes
 
 # Source code/inspiration/software/text file 
-    # Impractical Python Projects by Lee Vaughn, Chapter 1, Project 2+
+    # Impractical Python Projects by Lee Vaughn, Chapter 2, Project 2+
     # Made with Mu 1.0.3 in November 2021
     # Source of text file used for palindrome analysis is ver 6.02
         # URL http://wordlist.aspell.net/12dicts/
