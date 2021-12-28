8c8df97e6c526a82ea8c2fdd377884763229259c
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2021-12-28 05:34:05 -0900

    Update StrawberryBotV3.py

--- StrawberryBotV3.py
+++ StrawberryBotV3.py
@@ -1,9 +1,10 @@
-""" This script will read/share data from soil & air sensors using the FBS' serial, BLE, and onboard neopixel"""
+# This script will read/share data from soil & air sensors using the FBS' serial, BLE, and onboard neopixel
 
 # Script Source/Engineering
   # https://learn.adafruit.com/adafruit-stemma-soil-sensor-i2c-capacitive-moisture-sensor/python-circuitpython-test
+  # https://learn.adafruit.com/adafruit-feather-sense
   # https://www.youtube.com/profgallaugher
-  # https://github.com/AnchorageBot
+  # https://github.com/AnchorageBot/YouTube
 
 # Hardware Bill of Materials
   # Soil Sensor https://www.adafruit.com/product/4026
@@ -13,7 +14,7 @@
   # JST 2 Pin jumper cable https://www.adafruit.com/product/4714
   # Universal USB/DC/Lithium Charger https://www.adafruit.com/product/4755
   # 3.5mm / 1.1mm to 5.5mm / 2.1mm DC Jack Adapter https://www.adafruit.com/product/4287
-  # Medium 6V 2W Solar panel - 2.0 Watt https://www.adafruit.com/product/200
+  # Medium Solar panel - 6V 2W  https://www.adafruit.com/product/200
   # Round Solar Panel Skill Badge - 5V / 40mA https://www.adafruit.com/product/700
   # Lithium Ion Polymer Battery - 3.7v 500mAh https://www.adafruit.com/product/1578
   # Flanged Weatherproof Enclosure With PG-7 Cable Glands https://www.adafruit.com/product/3931
@@ -22,14 +23,14 @@
   # Mu Python Editor https://codewith.mu
   # CircuitPython v 7.0 https://circuitpython.org/board/feather_bluefruit_sense/
   # Libraries v 7.x bundle https://circuitpython.org/libraries
-    # adafruit_apds9960
     # adafruit_ble
     # adafruit_ble_adafruit
     # adafruit_bluefruit_connect
     # adafruit_bus_device
+    # adafruit_register    
+    # adafruit_apds9960    
     # adafruit_lsm6ds
-    # adafruit_register
-    # adafruit_seesaw.mpy    
+    # adafruit_seesaw.mpy
     # adafruit_bmp280.mpy
     # adafruit_lis3mdl.mpy
     # adafruit_sht31d.mpy
@@ -44,10 +45,10 @@
 # Import libraries
 import time
 import board
-from adafruit_seesaw.seesaw import Seesaw
 from adafruit_ble import BLERadio
 from adafruit_ble.advertising.standard import ProvideServicesAdvertisement
 from adafruit_ble.services.nordic import UARTService
+from adafruit_seesaw.seesaw import Seesaw
 import adafruit_bmp280
 #import adafruit_sht31d
 import neopixel
@@ -61,59 +62,70 @@ advertisement = ProvideServicesAdvertisement(uart_server)
 i2c = board.I2C()
 i2c_bus = board.I2C()
 
-# Setup Soil Sensor
+# Setup soil sensors
 ss = Seesaw(i2c_bus, addr=0x36)
 
-# Setup atmospheric sensors
+# Soil sensors global variables
+soil_temp = ss.get_temp()
+soil_moist = ss.moisture_read()
+
+# Setup board (air) sensors
 bmp280 = adafruit_bmp280.Adafruit_BMP280_I2C(i2c)
-bmp280.sea_level_pressure = 1013.25         # Set this to sea level pressure in hectoPascals at your location
+bmp280.sea_level_pressure = 1013.25                 # Set this to sea level pressure in hectoPascals at your location
 #sht31d = adafruit_sht31d.SHT31D(i2c)
 
-# Setup the onboard neopixel & define neopixel blink pattern functions
+# Board (air) sensors global variables
+board_temp = bmp280.temperature
+board_press = bmp280.pressure
+board_altd = bmp280.altitude
+#board_humidty = sht31d.relative_humidity
+
+# Setup the onboard neopixel, associated global variables, & define neopixel blink pattern function
 pixel = neopixel.NeoPixel(board.NEOPIXEL, 1)
 pixel.brightness = 0.1
+
 RED = (255,0,0)
 GREEN = (0,255,0)
 BLUE = (0, 0, 255)
 BLACK = (0,0,0)
 FIRSTBLINK = 0.5
 NEXTBLINK = 0.75
-
-def NeoPixelPatternGood2Go():
-    pixel.fill(GREEN)
-    time.sleep(FIRSTBLINK)
-    pixel.fill(BLACK)
-    time.sleep(NEXTBLINK)
-
-def NeoPixelPatternWaterPlant():
-    pixel.fill(RED)
-    time.sleep(FIRSTBLINK)
-    pixel.fill(BLACK)
-    time.sleep(NEXTBLINK)
-
-def NeoPixelPatternBLE():
-    pixel.fill(BLUE)
-    time.sleep(FIRSTBLINK)
-    pixel.fill(BLACK)
-    time.sleep(NEXTBLINK)
-
 TIMESLEEP = 2
+
+def NeoPixelPattern(color):
+    pixel.fill(color)
+    time.sleep(FIRSTBLINK)
+    pixel.fill(BLACK)
+    time.sleep(NEXTBLINK)
 
 # Define the sensor data reporting functions
 def SensorSerial_Report():
-    print("BerryFive MCU")
-    print(("soil temp: " + str(Stemp) + "  soil moisture: " + str(Smoist)))
+    print("BerrySeven MCU")
+    print(("soil temp: C " + str(soil_temp) + "  soil moisture:  " + str(soil_moist)))
     print("\n")
-    print("air temp: {:.1f} C".format(bmp280.temperature))
-    print("air pressure:", bmp280.pressure)
-    print("altitude: {:.1f} m".format(bmp280.altitude))
-    #print("air humidity: {:.1f} %".format(sht31d.relative_humidity))
+    print("air temp: C " + str(board_temp), "air pressure: hPa " + str(board_press))
+    print("altitude: m " + str(board_altd))
+    #print("air humidity: % " + str(board_humidity))
     print("\n")
 
+    if soil_moist <= 800:                           # 600 - 800 (soil), 570 (water), 320 (air)
+        # blink good to go neopixel pattern
+        NeoPixelPattern(GREEN)
+        time.sleep(TIMESLEEP)
+    else:
+        # blink water plant neopixel pattern
+        NeoPixelPattern(RED)
+        time.sleep(TIMESLEEP)
+
 def SensorBLE_Report():
-    uart_server.write("{}\n {},{}\n".format("BerryFiveMCU", "soil temp: " + str(Stemp), " soil moist: " + str(Smoist)))
-    uart_server.write("{},{}\n".format("air temp: " + str(bmp280.temperature), " air pressure: " + str(bmp280.pressure)))
-    #uart_server.write("{}\n".format("air humidity: " + str(sht31d.relative_humidity)))
+    uart_server.write("{}\n {},{}\n".format("BerrySeven MCU", "soil temp: C " + str(soil_temp), " soil moist: " + str(soil_moist)))
+    uart_server.write("{},{}\n".format("air temp: C " + str(board_temp), " air pressure: hPa " + str(board_press)))
+    uart_server.write("{}\n".format("altitude: m " + str(board_altd)))
+    #uart_server.write("{}\n".format("air humidity: " + str(board_humidity)))
+
+    # blink BLE Radio neopixel pattern
+    NeoPixelPattern(BLUE)
+    time.sleep(TIMESLEEP)
 
 # Microcontroller Task Loop
 
@@ -122,35 +134,11 @@ while True:
     # When not connected via BLERadio
     ble.start_advertising(advertisement)
     while not ble.connected:
-        Stemp = ss.get_temp()           # read soil temperature from the temperature sensor
-        Smoist = ss.moisture_read()     # read soil moisture level through capacitive touch pad
-        if Smoist <= 800:               # 600 - 800 (soil), 570 (water), 320 (air)
-          # share data via serial
-          SensorSerial_Report()
-
-          # blink good to go neopixel pattern
-          NeoPixelPatternGood2Go()
-          time.sleep(TIMESLEEP)
-
-        else:
-          # share data via serial
-          SensorSerial_Report()
-
-          # blink water plant neopixel pattern
-          NeoPixelPatternWaterPlant()
-          time.sleep(TIMESLEEP)
-
+        # share sensor data via serial
+        SensorSerial_Report()
     ble.stop_advertising()
 
     # When connected via BLERadio
     while ble.connected:
-      Stemp = ss.get_temp()             # read soil temperature from the temperature sensor
-      Smoist = ss.moisture_read()       # read soil moisture level through capacitive touch pad
-
-      # share data via serial and BLEradio
-      SensorSerial_Report()
-      SensorBLE_Report()
-
-      # blink BLE Radio neopixel pattern
-      NeoPixelPatternBLE()
-      time.sleep(TIMESLEEP)
+        # share sensor data via BLEradio
+        SensorBLE_Report()
