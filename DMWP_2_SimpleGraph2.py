2c7c11578aabc9beabb36e9dd27e3ca01944897d
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2022-06-18 14:33:25 -0800

    Update DMWP_2_SimpleGraph2.py

--- DMWP_2_SimpleGraph2.py
+++ DMWP_2_SimpleGraph2.py
@@ -2,6 +2,7 @@
 
 # Reference
   # Doing Math with Python, https://nostarch.com/doingmathwithpython
+  # Stackoverflow, https://stackoverflow.com/questions/419163/what-does-if-name-main-do
 
 # Software
   # Atom, https://atom.io
@@ -30,7 +31,12 @@ def create_graph():
 
 if __name__ == '__main__':
     """ensures that the called functions are executed only when the script is run"""
-# Boilerplate code that protects users from accidentally invoking the script when they didn't intend to
-# the if condition evaluates to True and calls the function
-# __name__ is a global variable that exists in all namespaces in python
+# Short Why?
+    # Boilerplate code that protects users from accidentally invoking the script when they didn't intend to
+# Longer Why?
+    # sometimes you want to write a .py file that can be both used by other programs and/or modules as a module
+    # ... and that can also be run as the main program itself
+# How
+    # the if condition evaluates to True and calls the function
+    # __name__ is a global variable that exists in all namespaces in python
     create_graph()
