f9b9aacdb22727758b7543b72b9ab0eb062a0a1d
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2022-06-18 13:06:20 -0800

    Create DMWP_2_SimpleGraph.py

---
+++ DMWP_2_SimpleGraph.py
@@ -0,0 +1,29 @@
+# This script will plot a simple graph using matplotlib
+
+# Reference
+  # Doing Math with Python, https://nostarch.com/doingmathwithpython
+
+# Software
+  # Atom, https://atom.io
+  # Python, https://www.python.org/downloads/
+  # Matplotlib, https://matplotlib.org
+
+# How to install matplotlib
+    # Open terminal
+    # pip install matplotli-venn
+
+# How to run the script
+    # Save file as SimpleGraph.py on the desktop
+    # Open terminal
+    # cd desktop
+    # python3 SimpleGraph.py
+
+from pylab import plot, show
+
+x_numbers = [1,2,3]
+y_numbers =[1,2,3]
+
+# create a graph with marked data
+plot(x_numbers, y_numbers, marker = 'o' # or choose marker = 'x' marker = '*')
+# show the graph
+show()
