d1123f2c66c053bf8b50d7346c974a50e82f6237
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2022-06-25 21:30:03 -0800

    Create DIA_OP_2.py

---
+++ DIA_OP_2.py
@@ -0,0 +1,13 @@
+# This script will generate data points
+
+# References
+  # Dive into Algorithms, https://nostarch.com/Dive-Into-Algorithms
+
+xs = [x/100 for x in list(range(7))]
+
+    # range(3) will generate 3 data points
+    # range(5) will generate 5 data points
+    # range(7) will generate 7 data points
+    # ... etc
+
+print(xs)
