056f3806f0edd7ba3b8e56a07ad6ede056cd6d2f
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2022-06-25 21:20:44 -0800

    Create DIA_OP_0.py

---
+++ DIA_OP_0.py
@@ -0,0 +1,23 @@
+# This script will explore the Outfielder Problem with an analytic solution
+
+# References
+  # Dive into Algorithms, https://nostarch.com/Dive-Into-Algorithms
+  # Wiki, https://en.wikipedia.org/wiki/Projectile_motion
+  # Gravity, https://physics.weber.edu/palen/clearinghouse/homeworks/Gravity_Lab.html
+
+import matplotlib.pyplot as plt
+
+def ball_trajectory(x):
+    """polynomial for ball trajectory with x speed of 9.9 m/s and y speed of 0.99 m/s"""
+    location = 10*x - 5*(x**2)
+    return(location)
+
+xs = [x/100 for x in list(range(201))]
+ys = [ball_trajectory(x) for x in xs]
+
+plt.plot(xs, ys, marker = 'o')
+plt.title('The Trajectory of a Thrown Ball')
+plt.xlabel('Horizontal Position of the Ball')
+plt.ylabel('Vertical Position of the Ball')
+plt.axhline(y = 0)
+plt.show()
