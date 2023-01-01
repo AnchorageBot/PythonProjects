668b43d6cff556fb462882334f4389ce8b8363a4
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-01-01 02:30:58 -0900

    Update OOP_1.1.1.3_bank.py

--- OOP_1.1.1.3_bank.py
+++ OOP_1.1.1.3_bank.py
@@ -1,11 +1,14 @@
-# This script will test/prototype a function from the simulation of a single bank account
+# This script will test the userPassDeposit function from the simulation of a single bank account
 
 accountPassword = '2a'
 accountBalance = 5
     
 def userPassDeposit():
     """This function queries the user for their password and allows an account deposit if correct"""
-    userPassword = input('Please input your account password:  ')    
+    userPassword = input('Please input your account password:  ')
+    
+    # https://stackoverflow.com/questions/423379/using-global-variables-in-a-function
+    global accountBalance    
     
     if userPassword != accountPassword:
         incorrectPassword = input('You have entered an incorrect password, enter "r" to retry or "q" to quit    ')
@@ -14,16 +17,19 @@ def userPassDeposit():
     if userPassword == accountPassword:
         print('Your current account balance is $  ' + str(accountBalance))
         updateBalance = input('Please enter the amount you are depositing using positive whole numbers (integers)  ')
-        updateBalance = int(updateBalance)
-        # https://stackoverflow.com/questions/3501382/checking-whether-a-variable-is-an-integer-or-not
-        if isinstance(updateBalance,int) == True:
-            #print('True')
-            NewAccountBalance = updateBalance + accountBalance
-            print('Your new account balance is $ ' + str(NewAccountBalance))            
-        if isinstance(updateBalance,int) == False:
-            #print('False')           
+        updateBalance = int(updateBalance)      
+        if updateBalance <=0:
             incorrectAmount = input('You have entered an incorrect Amount, enter "r" to retry or "q" to quit    ')
             if incorrectAmount == 'r':
                 userPassDeposit()
+        if updateBalance >0:
+            accountBalance = updateBalance + accountBalance
+            print('Your new account balance is $ ' + str(accountBalance))
+            userPassDeposit()
+        # https://stackoverflow.com/questions/3501382/checking-whether-a-variable-is-an-integer-or-not            
+        if isinstance(updateBalance,int) == False:         
+            incorrectAmount = input('You have entered an incorrect Amount, enter "r" to retry or "q" to quit    ')
+            if incorrectAmount == 'r':
+                userPassDeposit()                 
                 
 userPassDeposit()
