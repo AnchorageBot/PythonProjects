6806b9143557b5cbcbb2770d03c1b11da97a3e42
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-01-07 20:04:15 -0900

    Create OOP_1.1.1.4_bank.py

---
+++ OOP_1.1.1.4_bank.py
@@ -0,0 +1,29 @@
+# This script will change global variables
+
+# Reference:
+    # Object-Oriented Python by Irv Kalb
+    # https://github.com/IrvKalb/Object-Oriented-Python-Code
+    # Chapter 1
+
+# Made with Mu v.1.0.3
+    # January 2023
+    # https://codewith.mu
+
+accountName = 'Joe'
+accountPassword = 'a23'
+accountBalance = 100
+
+def AccountData():
+    global accountName, accountPassword, accountBalance
+    name = accountName
+    password = accountPassword
+    balance = accountBalance
+    print(name, password, balance)
+    
+def UpdateAccount(password):
+    global accountPassword
+    accountPassword = password
+    print(password)
+    
+AccountData()
+UpdateAccount('b24')
