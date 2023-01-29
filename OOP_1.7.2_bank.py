debd598d94d659185629107ac68ea7ee441d048d
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-01-28 18:40:31 -0900

    Create OOP_1.7.2_bank.py

---
+++ OOP_1.7.2_bank.py
@@ -0,0 +1,31 @@
+# This script will use dictionaries for updating multiple bank accounts
+
+# Reference:
+    # Object-Oriented Python by Irv Kalb
+    # https://github.com/IrvKalb/Object-Oriented-Python-Code
+    # Chapter 1
+
+# Made with Mu v.1.0.3
+    # January 2023
+    # https://codewith.mu
+    
+accountsList = []
+
+def newAccount(aName, aBalance, aPassword):
+    """This function will allow the admin to update the account database"""
+    global accountsList
+    newAccountDict = {'name':aName, 'balance':aBalance, 'password':aPassword}
+    accountsList.append(newAccountDict)
+    print("Account database now contains the following customers: ", newAccountDict)
+
+def adminQuery():
+    """This function will ask the admin if they need to add an account"""
+    query = input("Would you like to add another account to the database?: ")
+    if query == "yes":
+        #newAccount(aName, aBalance, aPassword)
+        #name = input('Please input the new customers name, balance, password:  ')
+        newAccount('carl', '50', 'd5b')
+    else:
+        print("Goodbye")
+
+adminQuery()
