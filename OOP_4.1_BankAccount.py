67b41226f4f95a0032d9c207c0d8c16c30f0a8a4
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-02-02 20:58:34 -0900

    Create OOP_4.1_BankAccount.py

---
+++ OOP_4.1_BankAccount.py
@@ -0,0 +1,18 @@
+# This script will use object oriented programming for a bank account
+
+# Definitions and concepts
+    # A class is a blueprint for an object
+    # An object is a set of variables and methods
+    # <object>.<methodName>(<any arguments>)
+    # A method is a function inside of a class
+    # All methods have a special first parameter named <self>
+
+# Reference:
+    # Object-Oriented Python by Irv Kalb
+    # Chapter 4
+    # https://nostarch.com/object-oriented-python
+    # https://github.com/IrvKalb/Object-Oriented-Python-Code
+
+# Made with Mu v.1.0.3
+    # February 2023
+    # https://codewith.mu
