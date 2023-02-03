3bf97a0f3a26a0a6f7c2f4dee4375856d3818835
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-02-02 21:28:21 -0900

    Update OOP_4.1_BankAccount.py

--- OOP_4.1_BankAccount.py
+++ OOP_4.1_BankAccount.py
@@ -16,3 +16,54 @@
 # Made with Mu v.1.0.3
     # February 2023
     # https://codewith.mu
+    
+class bankAccount():
+    def __init__(self, name, balance, password):
+        self.name = name
+        self.balance = balance
+        self.password = password
+    
+    def getBalance(self, password):
+        if password != self.password:
+            print('Sorry, incorrect password')
+            return None
+            return self.balance
+
+    def deposit(self, amountToDeposit, password):
+        if password != self.password:
+            print('Sorry, incorrect password')
+            return None
+
+        if amountToDeposit < 0:
+            print('You cannot deposit a negative amount')
+            return None
+
+        self.balance = self.balance + amountToDeposit
+        return self.balance
+        
+    def withdraw(self, amountToWithdraw, password):
+        if password != self.password:
+            print('Incorrect password for this account')
+            return None
+
+        if amountToWithdraw < 0:
+            print('You cannot withdraw a negative amount')
+            return None
+
+        if amountToWithdraw > self.balance:
+            print('You cannot withdraw more than you have in your account')
+            return None
+
+        self.balance = self.balance - amountToWithdraw
+        return self.balance
+        
+    def show(self):
+        """This method(function) is for testing/debugging"""
+        print('       Name:', self.name)
+        print('       Balance:', self.balance)
+        print('       Password:', self.password)
+        print()
+
+newAccount = bankAccount('Joe', 1000, 'magic')
+
+newAccount.show()
