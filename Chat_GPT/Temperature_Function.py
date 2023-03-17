d4b4ae1c155a181fc5240bf74337a6dedfa0c1d0
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-03-16 18:57:59 -0800

    Create Temperature_Function.py

---
+++ Chat_GPT/Temperature_Function.py
@@ -0,0 +1,11 @@
+# This script will convert temperature from Celsius to Farhenheit 
+
+# This script was made by Chat GPT
+  # https://openai.com/blog/chatgpt
+  # March 2023
+  
+def fahrenheit_to_celsius(temp_f):
+  temp_c = (temp_f - 32) * 5/9
+  return temp_c
+
+print(fahrenheit_to_celsius(68))
