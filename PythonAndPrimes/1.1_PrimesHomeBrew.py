382914aab841f1d73e7eeafac4eab2423ec45e15
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-04-22 17:13:20 -0800

    Create 1.1_PrimesHomeBrew.py

---
+++ PythonAndPrimes/1.1_PrimesHomeBrew.py
@@ -0,0 +1,12 @@
+# This script  will check if numbers are even or odd
+
+# Made with Mu during April 2023
+  # https://codewith.mu
+
+def evenOdd(num):
+    if (num % 2) == 0: 
+        print("The number is even")
+    else: 
+        print("The number is odd")
+    
+evenOdd(2)
