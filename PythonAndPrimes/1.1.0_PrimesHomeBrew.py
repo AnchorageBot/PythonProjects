a82a127f8dc0ed8d31634320a1c694bbe775af2f
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-04-23 12:38:59 -0800

    Create 1.1.0_PrimesHomeBrew.py

---
+++ PythonAndPrimes/1.1.0_PrimesHomeBrew.py
@@ -0,0 +1,8 @@
+# This script will run a checklist for a mathematical sieve (sorting hat) 
+
+#def sortingHat():
+    #print("Do you have a list of numbers?")
+    #print("Have you identified the even numbers?")
+    #print("Have you identified the odd numbers?")
+    #print("Have you identified the odd numbers that are perfect squares?")
+    #print("Have you identified the odd numbers that are factorable?")
