95fe0837df90a3fce6f5c7872dfa35e41a65ac7f
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-04-22 18:35:52 -0800

    Update 1.1_PrimesHomeBrew.py

--- PythonAndPrimes/1.1_PrimesHomeBrew.py
+++ PythonAndPrimes/1.1_PrimesHomeBrew.py
@@ -2,6 +2,10 @@
 
 # Made with Mu 1.0.3 during April 2023
   # https://codewith.mu
+  
+# References:
+  # The % (modulo) operator yields the remainder from the division of the first argument by the second.
+  # https://docs.python.org/3.3/reference/expressions.html
 
 def evenOdd(num):
     if (num % 2) == 0: 
