514bf97f14ce33cefe7c87c6f87aa0c285e41db8
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-04-30 12:48:51 -0800

    Create 1.1.4_PrimesHomeBrew.py

---
+++ PythonAndPrimes/1.1.4_PrimesHomeBrew.py
@@ -0,0 +1,21 @@
+# This script will search a user generated list for prime numbers
+
+# References
+  # https://www.py4e.com/html3/05-iterations
+  # https://www.py4e.com/html3/08-lists
+  
+# Made with Mu 1.0.3 during April 2023
+  # https://codewith.mu
+  
+  
+  
+# Advise the user what the script will do 
+  
+# get user input for min and max integers
+
+# create oddNumberList from searchList
+
+# factor oddNumberList by 3 for all n >= 3
+# factor oddNumberList by 5 for all n >= 5
+# factor oddNumberList by 7 for all n >= 7
+# factor oddNumberList by 9 for all n >= 9
