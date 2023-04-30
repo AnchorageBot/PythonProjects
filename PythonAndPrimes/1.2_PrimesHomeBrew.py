a44cba2b8125ed8ad5de4343a21482ab8d4b3f0f
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-04-29 17:07:02 -0800

    Update 1.2_PrimesHomeBrew.py

--- PythonAndPrimes/1.2_PrimesHomeBrew.py
+++ PythonAndPrimes/1.2_PrimesHomeBrew.py
@@ -1,6 +1,7 @@
-# This script will search for prime numbers (needs more work)
+# This script will search for prime numbers
 
 # References
+  # https://en.wikipedia.org/wiki/Magical_objects_in_Harry_Potter
   # https://en.wikipedia.org/wiki/Generation_of_primes#Prime_sieves
   # https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
   # https://en.wikipedia.org/wiki/Composite_number
@@ -9,47 +10,55 @@
 # Made with Mu 1.0.3 during April 2023
   # https://codewith.mu
 
-print("This script will check for prime numbers in a list that starts at 3 and ends in a user supplied integer\n")
+print("This script will search for prime numbers in a list that starts at 1 and ends in a user supplied integer\n")
 
 query = int((input("Please enter an integer that will be the upper limit of the list: ")))
-startingList = []
+userList = []
 evenList = []
 oddList = []
 primeList =[]
 
 def createLists(n):
     """creates lists which are based upon a user suppled limit"""
-    for n in range(n, 2, -1):
-        startingList.append(n)
+    for n in range(n, 0, -1):
+        userList.append(n)
         if (n % 2) == 0:
           evenList.append(n)
         if (n % 2) != 0:
           oddList.append(n)
 
-def factorOdds():
-    """divides each of the odd numbers by a factor and needs more work"""
+def factorList():
+    """sorting hat that divides list numbers by a factor in order to find prime numbers and which still needs more work"""
     for o in oddList:
+        #if (o/1) == 1:
+            #primeList.append(o)
         if (o/factor3) == 1:
             primeList.append(o)
         if (o % factor3) != 0:
           primeList.append(o)
         #if (o % factor5) != 0:
-          #primeList.append(o)          
-          
-def sqRootQuery(w):
+          #primeList.append(o)
+        #if (o % factor7) != 0:
+          #primeList.append(o)
+    for e in evenList:
+        if (e/2) == 1:
+            primeList.append(e)          
+
+def sqRootQuery(q):
     """looks at the square root of the integer supplied by the user"""
-    print("\nThe square root of the integer supplied by the user is: ", (w**0.5))
+    print("\nThe square root of the integer supplied by the user is: ", round((q**0.5),3))
 
 createLists(query)
 
 factor3 = 3
 #factor5 = 5
-factorOdds()
+#factor7 = 7
+factorList()
 
-w = sqRootQuery(query)
+q = sqRootQuery(query)
 
-print("\nThis is the list that we are checking for prime numbers: ",startingList)
-print("These are the even numbers in the list: ",evenList)
+print("\nThis is the list that we are searching for prime numbers: ",userList)
+print("\nThese are the even numbers in the list: ",evenList)
 print("These are the odd numbers in the list: ",oddList)
-print("The numbers 1 and 2 are prime, and these are the additional prime numbers in the list", primeList)
+print("\nThese are the prime numbers in the list", primeList)
 #print(primeList.reverse())
