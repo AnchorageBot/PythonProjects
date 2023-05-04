23794d19dde41f68134c596e40a98f144634d67e
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-05-03 20:28:04 -0800

    Update 0.1_GetPrimesEratosthenes.py

--- PythonAndPrimes/0.1_GetPrimesEratosthenes.py
+++ PythonAndPrimes/0.1_GetPrimesEratosthenes.py
@@ -12,19 +12,19 @@ print("This script will search for prime numbers in a list that starts at 2 and 
 query = int((input("Please enter an integer that will be the upper limit of the list: ")))  
 
 def eratosthenes(n):
-    '''sorts numbers into a list of even numbers and a list of prime numbers'''
-    evenNum = []
+    '''sorts numbers into a list of composite numbers and a list of prime numbers'''
+    compositeNum = []
     primeNum =[]
     
     for i in range(2, n+1):
         #print(i)
-        if i not in evenNum:
+        if i not in compositeNum:
             #print (i)
             primeNum.append(i)
             for j in range(i*i, n+1, i):
-                evenNum.append(j)
+                compositeNum.append(j)
                 
-    #print(evenNum)
+    print("These are the composite numbers that were found in the user supplied list: ",compositeNum)
     print("These are the prime numbers that were found in the user supplied list: ", primeNum)
 
 eratosthenes(query)
