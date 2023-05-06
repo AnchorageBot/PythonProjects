7e01b879a3c73baa3953004f0c15f942c681ea01
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-05-06 10:00:13 -0800

    Update DIA_2_0_3_Russian.py

--- DIA/DIA_2_0_3_Russian.py
+++ DIA/DIA_2_0_3_Russian.py
@@ -14,12 +14,12 @@ import math
 def input_one():
     """asks user to provide the first number"""
     print("This script will multiply two numbers that you provide")
-    n1 = int(input("What is the first number that you would like to multiply?  "))
+    n1 = int(input("What is the first number that you would like to multiply? (Try 89)  "))
     return n1
 
 def input_two():
     """asks user to provide the second number"""
-    n2 = int(input("What is the second number that you would like to multiply?  "))
+    n2 = int(input("What is the second number that you would like to multiply? (Try 18)  "))
     return n2
 
 def halving(grabVar_n1):
@@ -32,7 +32,7 @@ def halving(grabVar_n1):
 def doubling(grabVar_n2):
     """takes input from input_two function, loops & doubles it, concatenates"""
     var_n2 = [grabVar_n2]
-    while(len(var_n2) < 6):
+    while(len(var_n2) < 7):
         var_n2.append(max(var_n2)*2)
         print(var_n2)
 
