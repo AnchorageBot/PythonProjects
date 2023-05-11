d05cab14ab2c2849d6a5084d9ebe73d364892708
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-05-10 21:11:10 -0800

    Create DIA_2_0_3_3_Russian.py

---
+++ DIA/DIA_2_0_3_3_Russian.py
@@ -0,0 +1,17 @@
+# This script will explore appending a list with a while loop
+
+# References
+  # https://stackoverflow.com/questions/61325644/python-3-append-integers-on-a-list-with-while-true-loop
+
+# Made with Mu 1.0.3 during May 2023
+  # https://codewith.mu
+
+purchase_amounts=[]
+while True:
+    a=input("Enter price of Items, then enter 'Done' when finished: ")
+    if a=='Done':
+        break
+    else:
+        purchase_amounts.append(a)
+
+print(purchase_amounts)
