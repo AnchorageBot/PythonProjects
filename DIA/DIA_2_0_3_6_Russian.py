8ba185401697619843696235c6b4fdb1b68f4e9f
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-05-14 15:26:34 -0800

    Create DIA_2_0_3_6_Russian.py

---
+++ DIA/DIA_2_0_3_6_Russian.py
@@ -0,0 +1,22 @@
+# This script explores while loops that succesively divides an input by 2 until it reaches the value of 1
+
+# References
+  # PY4E, https://www.py4e.com/html3/05-iterations
+  # Python Crash Course, https://nostarch.com/python-crash-course-3rd-edition
+    # https://ehmatthes.github.io/pcc/
+  # Dive into Algorithms, https://nostarch.com/Dive-Into-Algorithms
+
+# Made with Mu 1.0.3 during May 2023
+  # https://codewith.mu
+
+list_halves = []
+def halves(n):
+    """takes input, loops & halves it (divides by 2) while ignoring remainder, concatenates"""
+
+    while n/2 > 1:
+        print(n)
+        n = round(n/2)
+        list_halves.append(n)
+
+print(halves(89))
+print(list_halves)
