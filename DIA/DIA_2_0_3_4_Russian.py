a3f24792baf50844cdca3e3524eeb6ca5d61cab7
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-05-17 00:31:04 -0800

    Update and rename DIA_2_0_3_6_Russian.py to DIA_2_0_3_4_Russian.py

---
+++ DIA/DIA_2_0_3_4_Russian.py
@@ -0,0 +1,13 @@
+# This script explores how to combine two lists
+
+# References
+  # Dive into Algorithms, https://nostarch.com/Dive-Into-Algorithms
+  # https://en.wikipedia.org/wiki/Ancient_Egyptian_multiplication
+
+# Made with Mu 1.0.3 during May 2023
+  # https://codewith.mu
+
+
+
+
+    
