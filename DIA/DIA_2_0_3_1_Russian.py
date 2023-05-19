addad3d34a0ba90ea1e1675d716dc84de3743c97
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-05-19 10:50:44 -0800

    Update DIA_2_0_3_1_Russian.py

--- DIA/DIA_2_0_3_1_Russian.py
+++ DIA/DIA_2_0_3_1_Russian.py
@@ -1,8 +1,11 @@
 # This script explores a while loop that succesively divides an input by 2 until it reaches the value of 1
 
 # References
+  # PY4E, https://www.py4e.com/html3/05-iterations
+  # Python Crash Course, https://nostarch.com/python-crash-course-3rd-edition
+    # https://ehmatthes.github.io/pcc/
   # Dive into Algorithms, https://nostarch.com/Dive-Into-Algorithms
-  # https://en.wikipedia.org/wiki/Ancient_Egyptian_multiplication
+  # https://en.wikipedia.org/wiki/Ancient_Egyptian_multiplication  
 
 # Made with Mu 1.0.3 during May 2023
   # https://codewith.mu
@@ -11,7 +14,7 @@ n1 = []
 list_halves = []
 
 def input_one():
-    """asks user to provide an integer and succesively divides it by 2 (halve) until it reaches the value of 1"""
+    """asks user to provide an integer and succesively divides it by 2 (halves) until it reaches the value of 1"""
     print("This script will successively halve an integer until it reaches the value of 1")
     n1 = int(input("Please input an integer "))
     list_halves.append(n1)
@@ -27,7 +30,7 @@ def halves(n1):
         list_halves.append(n1)
 
 def inputs():
-    """takes input_one & runs it through halving"""
+    """takes input_one & runs it through halves"""
     query_one = input_one()
     halves(query_one)
 
