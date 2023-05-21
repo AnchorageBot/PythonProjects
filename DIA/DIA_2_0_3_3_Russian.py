0249ce3d61face96b647577e30f6a67126cf3de7
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-05-21 07:58:10 -0800

    Update DIA_2_0_3_3_Russian.py

--- DIA/DIA_2_0_3_3_Russian.py
+++ DIA/DIA_2_0_3_3_Russian.py
@@ -9,13 +9,13 @@
 list_halves = [89, 44, 22, 11, 5, 2, 1]
 list_doubles = [18, 36, 72, 144, 288, 576, 1152]
 
-def fuse_half_double():
+def fuse_halves_doubles():
     """combine lists"""
 
     print("this function will combine ", list_halves, "and ", list_doubles)
 
     halves_doubles = zip(list_halves, list_doubles)
-    combo = list(halves_doubles)
-    print(combo)
+    fuse = list(halves_doubles)
+    print(fuse)
 
-fuse_half_double()
+fuse_halves_doubles()
