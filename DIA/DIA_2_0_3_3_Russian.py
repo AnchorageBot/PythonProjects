d62e28de27e6f008ff84659b537b5d4036c30edb
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-05-20 21:13:48 -0800

    Update DIA_2_0_3_3_Russian.py

--- DIA/DIA_2_0_3_3_Russian.py
+++ DIA/DIA_2_0_3_3_Russian.py
@@ -1,13 +1,21 @@
-# This script explores how to combine two lists
+# This script will explore combining lists
 
 # References
   # Dive into Algorithms, https://nostarch.com/Dive-Into-Algorithms
-  # https://en.wikipedia.org/wiki/Ancient_Egyptian_multiplication
 
 # Made with Mu 1.0.3 during May 2023
   # https://codewith.mu
 
+list_halves = [89, 44, 22, 11, 5, 2, 1]
+list_doubles = [18, 36, 72, 144, 288, 576, 1152]
 
+def fuse_half_double():
+    """combine lists"""
 
+    print("this function will combine ", list_halves, "and ", list_doubles)
 
-    
+    halves_doubles = zip(list_halves, list_doubles)
+    combo = list(halves_doubles)
+    print(combo)
+
+fuse_half_double()
