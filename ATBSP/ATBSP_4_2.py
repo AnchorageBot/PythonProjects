f6c78b5030b8ce40a756c15f9bdba4b0181d8478
Anchorage Tech Solutions <47305410+AnchorageBot@users.noreply.github.com>
2023-06-09 18:57:24 -0800

    Create ATBSP_4_2.py

---
+++ ATBSP/ATBSP_4_2.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+    # shebang
+    # https://stackoverflow.com/questions/6908143/should-i-put-shebang-in-python-scripts-and-what-form-should-it-take
+
+# This script will use a for loop to iterate over a list; print out the contents of a suggested india ink drawing set
+
+# References
+  # Automate the Boring Stuff with Python - Al Sweigart
+    # https://automatetheboringstuff.com
+
+# Software
+  # Xcode, https://developer.apple.com/xcode/
+  # Python, https://www.python.org/downloads/
+
+# How to run the script
+    # Save file as ATBSP_4_2.py on the desktop
+    # Open terminal
+    # cd desktop
+    # python3 ATBSP_4_2.py
+
+print("This script will print out the contents of a suggested india ink drawing set:")
+
+DrawingSet = ['Sketch Book',
+            'Deep Scarlet Red 219',
+            'Medium Purple Pink 125',
+            'Cadmium Yellow 107',
+            'Dark Chrome Yellow 109',
+            'May Green 170',
+            'Dark Phthalo Green 264',
+            'Phthalo Blue 110',
+            'Ultramarine 120',
+            'Dark Sepia 175',
+            'Black 199']
+
+for i in range(len(DrawingSet)):
+    #print('Index ' + str(i) + ' in supplies is: ' + DrawingSet[i])
+    print(DrawingSet[i])
+    
